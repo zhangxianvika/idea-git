caeaf34df0f68714337a860cc0ac9bfa40564b60
zhangsan <zhangsan@163.com>
2018-07-05 13:06:28 +0800

    第一次提交代码

---
+++ git-test44/src/cn/itcast/git/demo/GitTest.java
@@ -0,0 +1,9 @@
+package cn.itcast.git.demo;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git1");
+    }
+}
